f206cf8990284f63714be20ab455f3f18b895b8d
Deepika Tiwari <deepikatiwari92@gmail.com>
2020-09-11 11:39:08 +0200

    Instrument three private methods

---
+++ src/main/java/se/kth/castor/jitsi/plugin/MethodAspect8.java
@@ -0,0 +1,80 @@
+package se.kth.castor.jitsi.plugin;
+
+import org.glowroot.agent.plugin.api.*;
+import org.glowroot.agent.plugin.api.weaving.*;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+public class MethodAspect8 {
+    @Pointcut(className = "org.jitsi.videobridge.cc.BitrateController$TrackBitrateAllocation",
+            methodName = "getTargetBitrate",
+            methodParameterTypes = {},
+            timerName = "BitrateController$TrackBitrateAllocation - getTargetBitrate")
+    public static class MethodAdvice implements AdviceTemplate {
+        private static final int COUNT = 8;
+
+        private static int invocationCount;
+        private static final String methodFQN = MethodAdvice.class.getAnnotation(Pointcut.class).className() + "." +
+                MethodAdvice.class.getAnnotation(Pointcut.class).methodName();
+        private static final TimerName timer = Agent.getTimerName(MethodAdvice.class);
+        private static final String transactionType = "Pure";
+        private static String receivingObjectFilePath;
+        private static String paramObjectsFilePath;
+        private static String returnedObjectFilePath;
+        private static Logger logger = Logger.getLogger(MethodAdvice.class);
+
+        private static void setup() {
+            String[] fileNames = AdviceTemplate.setUpFiles(methodFQN);
+            receivingObjectFilePath = fileNames[0];
+            paramObjectsFilePath = fileNames[1];
+            returnedObjectFilePath = fileNames[2];
+        }
+
+        public static synchronized void writeMethodInvocationCount(String objectFilePath) {
+            try {
+                File file = new File(objectFilePath);
+                file.getParentFile().mkdirs();
+                FileWriter objectFileWriter = new FileWriter(file);
+                BufferedWriter bw = new BufferedWriter(objectFileWriter);
+                bw.write(methodFQN + ": " + invocationCount);
+                bw.newLine();
+                bw.flush();
+                bw.close();
+            } catch (Exception e) {
+                logger.info("MethodAspect" + COUNT);
+            }
+        }
+
+        @OnBefore
+        public static TraceEntry onBefore(OptionalThreadContext context,
+                                          @BindReceiver Object receivingObject,
+                                          @BindMethodName String methodName) {
+            setup();
+            ++invocationCount;
+            writeMethodInvocationCount(receivingObjectFilePath);
+            writeMethodInvocationCount(paramObjectsFilePath);
+            MessageSupplier messageSupplier = MessageSupplier.create(
+                    "className: {}, methodName: {}",
+                    MethodAdvice.class.getAnnotation(Pointcut.class).className(),
+                    methodName
+            );
+            return context.startTransaction(transactionType, methodName, messageSupplier, timer,
+                    OptionalThreadContext.AlreadyInTransactionBehavior.CAPTURE_NEW_TRANSACTION);
+        }
+
+        @OnReturn
+        public static void onReturn(@BindReturn Object returnedObject,
+                                    @BindTraveler TraceEntry traceEntry) {
+            writeMethodInvocationCount(returnedObjectFilePath);
+            traceEntry.end();
+        }
+
+        @OnThrow
+        public static void onThrow(@BindThrowable Throwable throwable,
+                                   @BindTraveler TraceEntry traceEntry) {
+            traceEntry.endWithError(throwable);
+        }
+    }
+}
